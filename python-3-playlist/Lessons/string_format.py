ad962e29d200d80b9d9af3b8e174f53afc1db0c2
burrelle <burrelle@oregonstate.edu>
2018-01-02 15:52:09 -0800

    Finished lesson 7

---
+++ python-3-playlist/Lessons/string_format.py
@@ -0,0 +1,12 @@
+num1 = 3.1415
+num2 = 10.234
+
+# Previous ways
+# print('Num1 is:', num1, "Num2 is:", num2)
+
+# Format method
+# print('Num1 is {} and numb2 is {}'.format(num1, num2))
+
+# Using f-string
+print(f'num1 is {num1} and num2 is {num2}')
+
