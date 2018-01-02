0d44a8cee40bbbd3ce0ce11ed320cd1db6531b4d
burrelle <burrelle@oregonstate.edu>
2018-01-02 15:09:40 -0800

    Completed through lesson 6

---
+++ python-3-playlist/area_calc.py
@@ -0,0 +1,11 @@
+# name = input('Tell me your name: ')
+# age = input("Age? ")
+# print("Hello " + name + " you are " + age)
+
+# Calculate the area of a circle
+radius = input("Enter the radius of your circle: ")
+area = 3.142 * int(radius) ** 2
+
+print("The area of the circle is: ", area)
+
+
