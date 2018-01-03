cd698dbb96a9a71fddb795fa107f3c51144decdd
burrelle <burrelle@oregonstate.edu>
2018-01-02 18:58:46 -0800

    Finished lesson 10.

--- python-3-playlist/Lessons/loops.py
+++ python-3-playlist/Lessons/loops.py
@@ -1,11 +1,19 @@
-ninjas = ['ryu', 'yoshi', 'crystal', 'steve']
+# ninjas = ['ryu', 'yoshi', 'crystal', 'steve']
 
 # for n in ninjas:
-#   print(n)
+#  print(n)
 
-# for n in ninjas[0:2]:
+#  for n in ninjas[0:2]:
 # 	print (n)
 
-for n in ninjas:
-	if n == 'steve':
-		print('Steve is a ninja.')
+# for n in ninjas:
+# 	if n == 'steve':
+# 		print('Steve is a ninja.')
+
+age = 25
+num = 0
+
+while num < age:
+  if num % 2 == 0:
+    print(num)
+  num += 1
