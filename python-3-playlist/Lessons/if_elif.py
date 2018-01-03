a8afecb5e7ee75610be738e5a7cc9b69ddd5b629
burrelle <burrelle@oregonstate.edu>
2018-01-02 16:10:09 -0800

    Finished lesson 8

---
+++ python-3-playlist/Lessons/if_elif.py
@@ -0,0 +1,15 @@
+# age = int(input('What is your age? '))
+
+# if age < 10:
+#   print('You are are not that old.') 
+# elif age < 40:
+#   print('Getting older day by day.')
+# else:
+#   print('You are wise beyond your years')
+
+response = input('Do you eat meat? (Y/N) ')
+
+if response == 'Y':
+  print('Here is the meat menu')
+elif response == 'N':
+  print('Here is the veggie menu')
