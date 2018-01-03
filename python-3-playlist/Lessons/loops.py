2e33e490aacde21b082c87ae96f1ac542a5dd43c
burrelle <burrelle@oregonstate.edu>
2018-01-02 16:19:55 -0800

    Finsihed lesson 9.

---
+++ python-3-playlist/Lessons/loops.py
@@ -0,0 +1,11 @@
+ninjas = ['ryu', 'yoshi', 'crystal', 'steve']
+
+# for n in ninjas:
+#   print(n)
+
+# for n in ninjas[0:2]:
+# 	print (n)
+
+for n in ninjas:
+	if n == 'steve':
+		print('Steve is a ninja.')
