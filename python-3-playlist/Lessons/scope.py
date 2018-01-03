09e8e5c9a5baf109f475a63ba0780fb596326bee
burrelle <burrelle@oregonstate.edu>
2018-01-03 14:25:41 -0800

    Finished lesson 13, variable scope

---
+++ python-3-playlist/Lessons/scope.py
@@ -0,0 +1,10 @@
+my_name = 'ryu'
+
+def print_name():
+  global my_name
+  my_name = 'yoshi'
+  print('The name inside the function is', my_name)
+
+print_name()
+print('Outside the function the name is', my_name)
+
