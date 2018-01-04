c6c19f4e8de3ca21b524ecaa90eaf2c0aa4e7c59
burrelle <burrelle@oregonstate.edu>
2018-01-04 11:12:52 -0800

    Lesson 20 - Bar tab calculator

---
+++ python-3-playlist/Projects/bar_tab.py
@@ -0,0 +1,32 @@
+class Tab:
+
+  menu = {
+    'wine' : 5,
+    'beer' : 3,
+    'soda' : 2,
+    'chicken' : 10,
+    'beef': 15,
+    'veggie': 12,
+    'dessert': 6
+  }
+
+  def __init__(self):
+    self.total = 0
+    self.items = []
+
+  def add(self, item):
+    self.items.append(item)
+    self.total += self.menu[item]
+  
+  def print_bill(self, tax, service):
+    tax = (tax/100) * self.total
+    service = (service/100) * self.total
+    total = self.total + tax + service
+
+    for i in self.items:
+      print(f'{i} ${self.menu[i]}')
+    
+    print(f'{"Total" 20} : ${total:.2f}')
+
+
+
