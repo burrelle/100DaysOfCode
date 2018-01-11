bd45a442ec3e70f636240f9eb1b5b5e329c167d3
burrelle <burrelle@oregonstate.edu>
2018-01-11 11:28:23 -0800

    mySQL and classes.

--- php-learning/index.php
+++ php-learning/index.php
@@ -1,12 +1,29 @@
 <?php
-
-$animals = ['dog', 'cat'];
-
-require 'functions.php';
-
-// dd($animals);
-echo ageVerify(12) . "\n";
-echo ageVerify(21) . "\n";
-echo ageVerify(45) . "\n";
-
-// require 'index.view.php';
+require "functions.php";
+/**
+* Task class
+*/
+class Task {
+	public $description;
+	public $completed = false;
+	public function __construct($description)
+	{
+		$this->description = $description;
+	}
+	public function complete()
+	{
+		$this->completed = true;
+	}
+	protected function isComplete ()
+	{
+		return $this->completed;
+	}
+}
+$tasks = [
+	new Task('Go to the store'),
+	new Task('Finish my screen cast'),
+	new Task('Clean the house')
+]; 
+$tasks[0]->complete();
+//dd($tasks);
+require 'index.view.php';
--- php-learning/index.view.php
+++ php-learning/index.view.php
@@ -5,25 +5,16 @@
   <title>Document</title>
 </head>
 <body>
-  <h1>Task for the day</h1>
   <ul>
-      <li>
-        <strong>Name: </strong><?= $task['title'] ?>
-      </li>
-      <li>
-        <strong>Due Date: </strong><?= $task['due'] ?>
-      </li>
-      <li>
-        <strong>Person Responsible: </strong><?= $task['assign_to'] ?>
-      </li>
-      <li>
-        <strong>Status: </strong>
-        <?php if($task['completed']) : ?>
-            <span>&#9989;</span>
-        <?php else: ?>
-            <span>Incomplete</span>          
-        <?php endif; ?>
-      </li>
-  </ul>
+		<?php foreach ($tasks as $task) : ?>
+			<li>
+				<?php if ($task->completed) : ?>
+					<strike><?= $task->description; ?></strike>
+				<?php else : ?>
+					<?= $task->description; ?> 
+				<?php endif; ?>
+			</li>
+		<?php endforeach; ?>
+	</ul>
 </body>
 </html>
