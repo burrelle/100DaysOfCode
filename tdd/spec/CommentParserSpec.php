bf5f4de554ea1fd3e312e26ffd29f972f35e94b2
burrelle <burrelle@oregonstate.edu>
2018-01-22 15:45:49 -0800

    Gulp file for TDD and refactoring red-green.

---
+++ tdd/spec/CommentParserSpec.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace spec;
+
+use CommentParser;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class CommentParserSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(CommentParser::class);
+    }
+
+    function it_parses_something()
+    {
+        $this->parse()->shouldReturn([]);
+    }
+}
